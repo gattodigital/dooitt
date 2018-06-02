d874ce516c5c09a564bfa57af3f1f5154d52d0ba
Gatto Digital <andy@gattodigital.com>
2018-06-02 00:12:59 -0500

    ...
    ...

--- front-end/src/app/auth-interceptor.service.ts
+++ front-end/src/app/auth-interceptor.service.ts
@@ -6,8 +6,8 @@ import { HttpInterceptor } from '@angular/common/http';
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req, next) {
     var authRequest = req.clone({
-      headers: req.headers.set('Authorization', 'token' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjBjYTQ5MmJmNzYzNjBlM2JjYTViMTUifQ.Yp8-FTf0xXabP5R9_j3NN97M8Ijf-sAroOWr6dFAAxc');
-    })
+      headers: req.headers.set('Authorization', 'token' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjBjYTQ5MmJmNzYzNjBlM2JjYTViMTUifQ.Yp8-FTf0xXabP5R9_j3NN97M8Ijf-sAroOWr6dFAAxc'),
+    });
     return next.handle(authRequest);
   }
 }
